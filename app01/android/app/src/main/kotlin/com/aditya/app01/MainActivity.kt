736ebd245f62146d078c95e04a05571b006bca98
gitadityakumar <ITSADITYASINGH05@GMAIL.COM>
2023-08-29 22:56:35 +0530

    Learnt Material App widget

---
+++ app01/android/app/src/main/kotlin/com/aditya/app01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aditya.app01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app01/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// The main function is starting point of all flutter App.
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
+      ),
+    ),
+  );
+}
